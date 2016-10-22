b7b2e9010c2b2a2f20313d5ed864ead1950ad0ec
little star <hxway007@163.com>
2016-10-22 22:29:57 +0800

    Add Program by cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EVENT_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher pub = new Publisher();
+            Subscriber sub = new Subscriber();
+
+            pub.NumberChanged += new NumerChangedEventHandler(sub.OnNumerChanged);
+            pub.DoSomething();
+            pub.NumberChanged(100);
+
+
+        }
+        public delegate void NumerChangedEventHandler(int count);
+        public class Publisher
+        {
+            private int count;
+           // public NumerChangedEventHandler NumberChanged; //声明委托变量
+           public event NumerChangedEventHandler NumberChanged; 
+            // 声明一个事件
+            public void DoSomething()
+            {
+                if (NumberChanged != null)
+                {
+                    count++;
+                    NumberChanged(count);
+                }
+            }
+        }
+
+        public class Subscriber
+        {
+            public void OnNumerChanged(int count)
+            {
+                Console.WriteLine("Subscriber notified: count = {0}", count);
+            }
+        }
+    }
+}
